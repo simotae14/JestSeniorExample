c06ff1a8796eb15cd7b3566be90a1c12c25a18c1
Simone Taeggi <startae14@gmail.com>
2020-01-20 23:49:15 +0100

    add new tests and a describe section

--- script.test.js
+++ script.test.js
@@ -5,14 +5,25 @@ dbMock = [
   'cheesepuff.com',
   'disney.com',
   'dogpictures.com'
-]
+];
 
-it('is a silly test', () => {
-  expect('hello').toBe('hello');
-  googleSearch('testtest', dbMock);
-});
+describe('googleSearch', () => {
+  it('is a silly test', () => {
+    expect('hello').toBe('hello');
+    googleSearch('testtest', dbMock);
+  });
 
-it('is searching google', () => {
-  expect(googleSearch('testtest', dbMock)).toEqual([]);
-  expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com']);
+  it('is searching google', () => {
+    expect(googleSearch('testtest', dbMock)).toEqual([]);
+    expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com']);
+  });
+
+  it('work with undefined and null input', () => {
+    expect(googleSearch(undefined, dbMock)).toEqual([]);
+    expect(googleSearch(null, dbMock)).toEqual([]);
+  });
+
+  it('does not return more than 3 matches', () => {
+    expect(googleSearch('com', dbMock).length).toEqual(3);
+  });
 });
